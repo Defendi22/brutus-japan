44ba8d6a0d2b0bf89a41322c1818d6eb292714b4
Defendi22 <fwdefendi20@gmail.com>
2026-06-23 00:30:42 -0300

    logo2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,7 +14,7 @@ const Header: React.FC<HeaderProps> = ({ onCartClick }) => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-20 flex items-center justify-between">
         <div className="flex items-center gap-2">
           <img 
-            src="logobrutussemfundo.png" 
+            src="assets/logobrutussemfundo.png" 
             alt="Logo Brutus" 
             className="h-25 w-auto"
           />
